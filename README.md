a123de4e38d85d76b95b7753207ace47c7e818c7
Gigen Thomas <gigenthomas@gmail.com>
2023-04-05 19:09:03 -0700

    adding ui-shared-layout

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Run `npx nx connect-to-nx-cloud` to enable [remote caching](https://nx.app) and 
 ## Further help
 
 Visit the [Nx Documentation](https://nx.dev) to learn more.
+
+
+
+nx generate @nrwl/react:library ui-shared-layout 
--- packages/prolog-app/pages/_app.tsx
+++ packages/prolog-app/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { NavigationProvider } from "@features/layout";
+import { NavigationProvider } from "@nx-example-org/ui-shared-layout";
 import { GlobalStyle } from "@nx-example-org/ui-shared";
 import { theme } from "@nx-example-org/ui-shared";
 
